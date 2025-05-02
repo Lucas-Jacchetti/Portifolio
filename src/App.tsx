5ac800315a1e1b2a08f3ef953e90bb7dc71840f3
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-05-02 01:35:05 -0300

    animações  e primeiro slide (arrumar sobra)

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 
 import Header from './components/header'
+import Name from './components/name'
 import './index.css'
 function App() {
 
   return (
     <>
       <Header />
-      
-      <body className='bg-ddarkblue font-futura'>
-        
+      <body className='font-futura bg-gradient-to-br from-ddarkblue from-25% via-gradientpink to-ddarkblue to-75% h-max w-max'>
+        <div className='flex flex-col'>
+          <Name/>
+          
+        </div>
       </body>
     </>
   )
